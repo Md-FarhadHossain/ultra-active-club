c9079cae06e4e0272fdc859ea1c472f5ddf2c526
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2022-09-29 22:40:13 +0600

    blog article added

---
+++ src/Blog/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Blog = (props) => {
+  return (
+    <div>
+        <h1>{props.title}</h1>
+        <p>{props.body}</p>
+    </div>
+  )
+}
+
+export default Blog
--- src/components/StudyPlan/StudyPlan.js
+++ src/components/StudyPlan/StudyPlan.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Blog from "../../Blog/Blog";
 import { getStoredTime } from "../../utilites/timeDB";
 import Activites from "../Activities/Activites";
 import UserActivity from "../UserActivity/UserActivity";
@@ -76,12 +77,30 @@ useEffect(() =>{
 
       <div className="studyClac-container">
         <UserActivity studyTime={studyTime} activities={activities} />
-
-
-
         
       </div>
+
+
+
+      {/* BLog section */}
+      <article className="blogArticle">
+       
+       <Blog title='1. How does React Works' body='React is a JavaScript library developed by Facebook which, among other things, was used to build Instagram.com. Its aim is to allow developers to easily create fast user interfaces for websites and applications alike. The main concept of React. js is virtual DOM.' />
+
+       <Blog title="2. Difference between props and state" body="The state is an updatable structure that is used to contain data or information about the component and can change over time. The change in state can happen as a response to user action or system event. Props are read-only components. It is an object which stores the value of attributes of a tag and work similar to the HTML attributes. It allows passing data from one component to other components." />
+
+
+       <Blog title="3. Use cases of useEffect" body="By using this Hook, you tell React that your component needs to do something after render. React will remember the function you passed we'll refer to it as our effect, and call it later after performing the DOM updates.f you have just made a new project using Create React App or updated to React version 18, you will notice that the useEffect hook is called twice in development mode." />
+
+      </article>
+
+      
     </section>
+
+
+
+
+
   );
 };
 
